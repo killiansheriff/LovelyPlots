8e3fd25934d48498b72fb7af0fba5b14afd94ed0
Killian Sheriff <ksheriff@mit.edu>
2022-07-25 07:25:04 -0700

    description

--- BeautifulPlots/utils.py
+++ BeautifulPlots/utils.py
@@ -2,6 +2,11 @@ import matplotlib as mpl
 
 
 def set_font(font_path):
+    """Set fonts for matplotlib plots
+
+    Args:
+        font_path (str): path to .tiff font
+    """
     font = mpl.font_manager.FontEntry(fname=font_path, name="my_font")
     mpl.font_manager.fontManager.ttflist.append(font)
 
@@ -12,7 +17,9 @@ def set_font(font_path):
     )
 
 
-def set_retina():
+def set_retina(): 
+    """set retina display support in IPython notebooks
+    """
     from IPython import get_ipython
 
     get_ipython().run_line_magic("matplotlib", "inline")
@@ -22,6 +29,14 @@ def set_retina():
 
 
 def set_size(subplots=(1, 1)):
+    """Set size for subplots
+
+    Args:
+        subplots (tuple, optional): number of (row,col) subplots. Defaults to (1, 1).
+
+    Returns:
+        tuple: size of subplots
+    """
 
     fig_width_in = mpl.rcParams["figure.figsize"][0]
     fig_height_in = mpl.rcParams["figure.figsize"][1] * (subplots[0] / subplots[1])
