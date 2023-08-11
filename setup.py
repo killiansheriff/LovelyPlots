3d47343200db8e71578f0eb76ca6e32e3d09677c
Killian Sheriff <ksheriff@mit.edu>
2023-08-11 21:45:22 +0200

    version bump

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ with open(os.path.join(root, "README.md"), "r", encoding="utf-8") as f:
 
 setup(
     name="LovelyPlots",
-    version="0.0.26",
+    version="0.0.27",
     packages=find_packages(exclude=["tests.*", "tests", "figs", "examples"]),
     author="Killian Sheriff",
     author_email="ksheriff@mit.edu",
